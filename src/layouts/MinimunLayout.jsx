47ee80b2268ca35099ffa8a7e2f6647a323cf0a6
Laxman Maharjan <hotmale_luksh@hotmail.com>
2024-06-01 14:48:45 +0545

    Routes

---
+++ src/layouts/MinimunLayout.jsx
@@ -0,0 +1,27 @@
+import { Box } from "@mui/material";
+import React from "react";
+import { Outlet } from "react-router-dom";
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+const MinimunLayout = () => {
+  return (
+    <>
+      <Header />
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+          margin: "1rem 0",
+        }}
+      >
+        <Outlet />
+      </Box>
+      <Footer />
+    </>
+  );
+};
+
+export default MinimunLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import "./App.css";
+import "./index.css";
+import GuestRoutes from "./routes/GuestRoutes.jsx";
+import MainRoutes from "./routes/MainRoutes.jsx";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+// Create client
+const queryClient = new QueryClient();
+
+// Create router
+const router = createBrowserRouter([...GuestRoutes, ...MainRoutes]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
